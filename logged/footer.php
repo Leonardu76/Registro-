11014ea5b3407cb5b940e55dde20b7081aec700d
LEONARDO OLIVEIRA SOUZA <88562663+Leonardu76@users.noreply.github.com>
2022-06-13 20:15:31 -0300

    Update footer.php

--- logged/footer.php
+++ logged/footer.php
@@ -1,12 +1,11 @@
 
-
 </body>
 <footer>
 
 <section id="blank"></section>
 <div class="container">
 <div id="social">
-            <a href="https://www.linkedin.com/in/leonardo-souza-3b0446176/" target="_blank"><i
+           <a href="https://www.linkedin.com/in/leonardo-oliveira-3b0446176/" target="_blank"><i
                     class="fab fa-linkedin"></i></a>
             <a href="https://api.whatsapp.com/send?phone=5531985808502" target="_blank"><i
                     class="fab fa-whatsapp-square"></i></a>
